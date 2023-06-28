39cb8dca04d5e88b5b2d1c7a4716e2786933aa4b
AppleInMyEye <940844742@qq.com>
2023-06-28 11:17:03 +0800

    Delete OJ/online-teaching-platform-master directory

--- OJ/online-teaching-platform-master/vuehouduan/src/main/java/com/yinhao/vuehouduan/common/RoleEnum.java
+++
@@ -1,5 +0,0 @@
-package com.yinhao.vuehouduan.common;
-
-public enum RoleEnum {
-    ROLE_ADMIN, ROLE_USER;
-}
--- OJ/online-teaching-platform-master/vuehouduan/src/main/java/com/yinhao/vuehouduan/mapper/FileMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yinhao.vuehouduan.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yinhao.vuehouduan.entity.Files;
-
-public interface FileMapper extends BaseMapper<Files> {
-}
